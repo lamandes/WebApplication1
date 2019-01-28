67f80da17ced855f2525406bc9861a7c6ec0c0ba
Andes Lam <lamandes@outlook.com>
2019-01-28 17:39:15 +0000

    MVC5 First App Project

---
+++ WebApplication1/Controllers/CustomerController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+using WebApplication1.ViewModels;
+
+namespace WebApplication1.Controllers
+{
+    public class CustomerController : Controller
+    {
+        // GET: Customer
+        public ActionResult Index()
+        {
+            var customerList = new List<Customer>
+            {
+                new Customer{ Id = 1, Name = "Andes"},
+                new Customer{ Id = 2, Name = "Mary"}
+            };
+            var viewModel = new CustomerViewModels
+            {
+                CustomerList = customerList
+            };
+            return View(viewModel);
+        }
+
+        [Route("Customer/Detail/{id}")]
+
+        // GET: Customer/Detail/xxxx
+        public ActionResult Detail(int id)
+        {
+            if (id > 2)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                if (id == 1)
+                {
+                    return View(new Customer { Id = 1, Name = "Andes" });
+                }
+                else
+                {
+                    return View(new Customer { Id = 2, Name = "Mary" });
+                }
+            }
+        }
+    }
+}
---
+++ WebApplication1/Controllers/MovieController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+using WebApplication1.ViewModels;
+
+namespace WebApplication1.Controllers
+{
+    public class MovieController : Controller
+    {
+
+        [Route("Movie")]
+        // GET: Movie
+        public ActionResult Movie()
+        {
+            var movieList = new List<Movie>
+            {
+                new Movie{ Name = "Shrek"},
+                new Movie{ Name = "Wall-e"}
+            };
+            var viewModel = new MovieViewModel
+            {
+                MovieList = movieList
+            };
+            return View(viewModel);
+        }
+    }
+}
